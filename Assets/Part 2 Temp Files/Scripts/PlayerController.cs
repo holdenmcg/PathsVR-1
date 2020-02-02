5c2787e5569814f2bc3d7f0b707d9e5aaa2c90ea
moocj <moocj@uw.edu>
2020-02-02 13:11:13 -0800

    Scene 2 setup

--- Assets/Part 2 Temp Files/Scripts/PlayerController.cs
+++ Assets/Part 2 Temp Files/Scripts/PlayerController.cs
@@ -39,24 +39,14 @@ public class PlayerController : MonoBehaviour {
             touch = Input.GetTouch(0);
         }
         if (Input.GetMouseButton(0) || touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary) {
-            CheckForNode();
             ShowTeleportLocation();
+            DrawCurve();
         } else {
             teleportIndicator.gameObject.SetActive(false);
         }
         if (Input.GetMouseButtonUp(0)) {
             TeleportToPoint();
-        }
-    }
-
-    void CheckForNode() { // TODO change it back to one raycast, not raycastall
-        RaycastHit hit;
-        if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit)) {
-            return;
-        }
-        if (hit.transform.gameObject.tag.Equals("Feature Node")) {
-            //GameObject UIGameObject = hit.transform.parent.GetComponent<Billboard>().ui;
-            //GameController.OpenMobileGUI(UIGameObject);
+            lineRenderer.positionCount = 0;
         }
     }
 
@@ -67,48 +57,40 @@ public class PlayerController : MonoBehaviour {
         // Only hit things w/ teleport environment mask
         if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit, maxRaycastDistance, environmentMask)) {
             teleportPos = Vector3.negativeInfinity;
-            Debug.Log("nope");
             teleportIndicator.gameObject.SetActive(false);
             return;
         }
-        MeshCollider meshCollider = (MeshCollider)hit.collider;
-
-
-        Mesh mesh = meshCollider.sharedMesh;
-        Vector3[] vertices = mesh.vertices;
-        int[] triangles = mesh.triangles;
-        Vector3 p0 = vertices[triangles[hit.triangleIndex * 3 + 0]];
-        Vector3 p1 = vertices[triangles[hit.triangleIndex * 3 + 1]];
-        Vector3 p2 = vertices[triangles[hit.triangleIndex * 3 + 2]];
-
 
         teleportPos = hit.point;
-
-        teleportIndicator.position = new Vector3(hit.point.x, hit.point.y + 1f, hit.point.z);
+        teleportIndicator.position = new Vector3(hit.point.x, hit.point.y + 0.05f, hit.point.z); // Adding a small value to y to reduce clipping
 
         Quaternion rotateTarget = Quaternion.LookRotation(Vector3.Cross(teleportIndicator.right, hit.normal));
         teleportIndicator.rotation = Quaternion.Lerp(teleportIndicator.rotation, rotateTarget, 0.2f); // TODO adjust lerp value
 
-        // TODO this is only for mobile where users will not be able to change their y rotation (side by side) when teleporting. In VR, users will have that ability
-        teleportIndicator.rotation = Quaternion.Euler(teleportIndicator.eulerAngles.x, cam.transform.eulerAngles.y, 0f); // Changing the y rotation to face the same direction as the camera
-
-        DrawCurve();
+        // TODO allow for y rotation (side by size) corresponding to controller orientation
+        teleportIndicator.rotation = Quaternion.Euler(teleportIndicator.eulerAngles.x, cam.transform.eulerAngles.y, teleportIndicator.eulerAngles.z); // Changing the y rotation to face the same direction as the camera
     }
 
     // Draws a quadratic bezier curve from the player to the teleport position
     void DrawCurve() {
+        lineRenderer.positionCount = curveSegmentCount;
         Vector3 startPoint = transform.position; // TODO this has to be at the hand position
         Vector3 controlPoint = Vector3.Lerp(transform.position, teleportPos, 0.5f);
-        controlPoint.y += 5f; // Arbitrary height on the curve
-        lineRenderer.SetPosition(0, startPoint + new Vector3(0, -1f, 0));
-        for (int i = 1; i < curveSegmentCount; i++) {
+        controlPoint.y += 10f; // Arbitrary height on the curve
+
+        lineRenderer.SetPosition(0, cam.transform.position);
+        for (int i = 1; i < lineRenderer.positionCount - 1; i++) {
             float t = (float)i / curveSegmentCount;
             Vector3 q0 = Vector3.Lerp(startPoint, controlPoint, t);
             Vector3 q1 = Vector3.Lerp(controlPoint, teleportPos, t);
             Vector3 q2 = Vector3.Lerp(q0, q1, t);
             lineRenderer.SetPosition(i, q2);
+
+            // Check intersection 
+
+
         }
-        lineRenderer.SetPosition(curveSegmentCount - 1, teleportPos);
+        lineRenderer.SetPosition(lineRenderer.positionCount - 1, teleportPos);
     }
 
     // Teleports the camera's GameObject to the defined teleport destination
@@ -125,7 +107,7 @@ public class PlayerController : MonoBehaviour {
         yield return new WaitForSeconds(0.3f);
         isTeleporting = false;
         transform.position = teleportPos;
-        transform.Translate(0, 10f, 0); // TODO temp adjustment for player height
+        transform.Translate(0, 5f, 0); // TODO temp adjustment for player height
     }
     
 
